8c44a4765aa87352f0cb19a5eb233fc659deb2b0
Ahmaddd99 <02achmadabdullatief@gmail.com>
2023-11-08 19:23:08 +0700

    add meta

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="robots" content="noindex,nofollow">
     <title>Login - Pemkab Bulungan</title>
     <link rel="stylesheet" href="{{ asset('assets/css/main/app.css') }}" />
     <link rel="stylesheet" href="{{ asset('assets/css/main/app-dark.css') }}" />
